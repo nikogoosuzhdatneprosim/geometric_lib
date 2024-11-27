5ac0bc2841df594950fd996fbed116d6a043438f
пряхин артем <sho8o@MacBook-Air-prahin.local>
2024-11-28 01:37:57 +0300

    Добавление unit-тестов.

--- circle.py
+++ circle.py
@@ -1,10 +1,81 @@
 import math
-
-
+import unittest
 def area(r):
+    """Возвращает площадь круга"""
+    # Параметры:
+    #   r (int): радиус круга
+    # Возвращаемое значение:
+    #   math.pi * r * r: искомая площадь круга
+    if type(r) is not int:
+        raise TypeError("Argument должен быть int")
+    if r <= 0:
+        raise ValueError("Argument должны быть больше нуля")
     return math.pi * r * r
 
-
 def perimeter(r):
+    """Возвращает периметр круга"""
+    # Параметры:
+    #   r (int): радиус круга
+    # Возвращаемое значение:
+    #   2 * math.pi * r: искомый периметр круга
+    if type(r) is not int:
+        raise TypeError("Argument должен быть int")
+    if r <= 0:
+        raise ValueError("Argument должны быть больше нуля")
     return 2 * math.pi * r
+class CircleAreaTestCases(unittest.TestCase):
+  def test_circle_int_first(self):
+    self.assertEqual(area(5), math.pi * 5 * 5)
+  def test_circle_int_second(self):
+    self.assertEqual(area(57285), math.pi * 57285 * 57285)
+  def test_circle_string_first(self):
+    with self.assertRaises(TypeError):
+      area("85")
+  def test_circle_string_second(self):
+    with self.assertRaises(TypeError):
+      area("ITMO")
+  def test_circle_bool_first(self):
+    with self.assertRaises(TypeError):
+      area(True)
+  def test_circle_bool_second(self):
+    with self.assertRaises(TypeError):
+      area(False)
+  def test_circle_negative_int_first(self):
+    with self.assertRaises(ValueError):
+      area(-5)
+  def test_circle_negative_int_second(self):
+    with self.assertRaises(ValueError):
+      area(-59853)
+  def test_circle_zero_int(self):
+    with self.assertRaises(ValueError):
+      area(0)
+class CirclePerimeterTestCases(unittest.TestCase):
+  def test_circle_int_first(self):
+    self.assertEqual(perimeter(7), 2 * math.pi * 7)
+  def test_circle_int_second(self):
+    self.assertEqual(perimeter(57285), 2 * math.pi * 57285)
+  def test_circle_string_first(self):
+    with self.assertRaises(TypeError):
+      perimeter("85")
+  def test_circle_string_second(self):
+    with self.assertRaises(TypeError):
+      perimeter("ITMO")
+  def test_circle_bool_first(self):
+    with self.assertRaises(TypeError):
+      perimeter(True)
+  def test_circle_bool_second(self):
+    with self.assertRaises(TypeError):
+      perimeter(False)
+  def test_circle_negative_int_first(self):
+    with self.assertRaises(ValueError):
+      perimeter(-5)
+  def test_circle_negative_int_second(self):
+    with self.assertRaises(ValueError):
+      perimeter(-59853)
+  def test_circle_zero_int(self):
+    with self.assertRaises(ValueError):
+      perimeter(0)
+
+if __name__ == "__main__":
+  unittest.main()
 
--- square.py
+++ square.py
@@ -1,7 +1,98 @@
-
+import unittest
 def area(a):
+    """Возвращает площадь квадрата"""
+    # Параметры:
+    #   a (int): длина квадрата
+    #   b (int): ширина квадрата  
+    # Возвращаемое значение:
+    #   a * a: искомая площадь квадрата
+    if type(a) is not int:
+        raise TypeError("Аргумент должен быть int")
+    if a <= 0:
+        raise ValueError("Аргумент должен быть больше нуля")
     return a * a
 
-
 def perimeter(a):
+    """Возвращает периметр квадрата"""
+    # Параметры:
+    #   a (int): длина квадрата
+    #   b (int): ширина квадрата
+    # Возвращаемое значение:
+    #   4 * a: искомый периметр квадрата
+    if type(a) is not int:
+        raise TypeError("Аргумент должен быть int")
+    if a <= 0:
+        raise ValueError("Аргумент должен быть больше нуля")
     return 4 * a
+class SquareAreaTestCases(unittest.TestCase):
+    def test_square_int_first(self):
+        self.assertEqual(area(5), 5 * 5)
+
+    def test_square_int_second(self):
+        self.assertEqual(area(57285), 57285 * 57285)
+
+    def test_square_string_first(self):
+        with self.assertRaises(TypeError):
+            area("85")
+
+    def test_square_string_second(self):
+        with self.assertRaises(TypeError):
+            area("ITMO")
+
+    def test_square_bool_first(self):
+        with self.assertRaises(TypeError):
+            area(True)
+
+    def test_square_bool_second(self):
+        with self.assertRaises(TypeError):
+            area(False)
+
+    def test_square_negative_int_first(self):
+        with self.assertRaises(ValueError):
+            area(-5)
+
+    def test_square_negative_int_second(self):
+        with self.assertRaises(ValueError):
+            area(-59853)
+
+    def test_square_zero_int(self):
+        with self.assertRaises(ValueError):
+            area(0)
+class SquarePerimeterTestCases(unittest.TestCase):
+    def test_square_int_first(self):
+        self.assertEqual(perimeter(9), 4 * 9)
+
+    def test_square_int_second(self):
+        self.assertEqual(perimeter(394732), 4 * 394732)
+
+    def test_square_string_first(self):
+        with self.assertRaises(TypeError):
+            perimeter("85")
+
+    def test_square_string_second(self):
+        with self.assertRaises(TypeError):
+            perimeter("ITMO")
+
+    def test_square_bool_first(self):
+        with self.assertRaises(TypeError):
+            perimeter(True)
+
+    def test_square_bool_second(self):
+        with self.assertRaises(TypeError):
+            perimeter(False)
+
+    def test_square_negative_int_first(self):
+        with self.assertRaises(ValueError):
+            perimeter(-5)
+
+    def test_square_negative_int_second(self):
+        with self.assertRaises(ValueError):
+            perimeter(-59853)
+
+    def test_square_zero_int(self):
+        with self.assertRaises(ValueError):
+            perimeter(0)
+
+if __name__ == '__main__':
+    unittest.main()
+
